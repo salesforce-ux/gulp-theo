6af564da6f1f18ac07e7379cfd331a4ac3b59833
Kaelig <kaelig@deloumeau.fr>
2017-01-26 21:42:19 -0800

    Hello, gulp-theo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # gulp-theo
+
+```js
+const gulp = require('gulp')
+const theo = require('theo')
+
+// Transform design/props.json to dist/props.scss:
+gulp.src('design/props.json')
+  .pipe(theo.plugin({
+    transform: { type: 'web' },
+    format: { type: 'scss' }
+  }))
+  .pipe(gulp.dest('dist'))
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2015, salesforce.com, inc. - All rights reserved
+// Licensed under BSD 3-Clause - https://opensource.org/licenses/BSD-3-Clause
+
+const through = require('through2')
+const gutil = require('gulp-util')
+const theo = require('theo')
+const Immutable = require('immutable')
+
+module.exports = Object.assign(
+  {},
+  // Export all native Theo methods
+  theo,
+  // Plugin:
+  {
+    plugin: function (options) {
+      return through.obj(function (file, enc, callback) {
+        if (file.isStream()) {
+          const err = this.emit('error', new gutil.PluginError('gulp-theo', 'Streaming not supported'))
+          return callback(err)
+        }
+
+        const opts = Immutable.fromJS(options)
+          .setIn(['transform', 'file'], file.path)
+          .setIn(['transform', 'data'], file.contents.toString('utf8'))
+          .toJS()
+
+        theo.convert(opts)
+          .then(res => {
+            file.path = file.path.replace(/(json|yml)$/, options.format.type)
+            file.contents = new Buffer(res)
+            callback(null, file)
+          })
+          .catch(e => {
+            const err = new gutil.PluginError('gulp-theo', e, { trace: true })
+            callback(err, file)
+          })
+      })
+    }
+  }
+)
