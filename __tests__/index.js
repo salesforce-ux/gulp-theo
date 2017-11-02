9a75eac0071c17d28333101bb4d927e6d52afae2
Adam Putinski <adam.putinski@gmail.com>
2017-11-02 16:55:36 -0400

    Remove immutable dependency

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,99 +1,104 @@
 // Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
-const gulp = require('gulp')
-const through = require('through2')
-const theo = require('../')
+const gulp = require('gulp');
+const through = require('through2');
+const theo = require('../');
 
 describe('gulp-theo', () => {
-  it('transforms Design Tokens as Sass', (done) => {
-    gulp.src('__fixtures__/a.json')
-      .pipe(theo.plugin(
-        {
+  it('transforms Design Tokens as Sass', done => {
+    gulp
+      .src('__fixtures__/a.json')
+      .pipe(
+        theo.plugin({
           transform: {
             type: 'web'
           },
           format: {
             type: 'scss'
           }
-        }
-      ))
-      .pipe(
-        through.obj((file, enc, next) => {
-          expect(file.contents.toString('utf8')).toMatchSnapshot()
-          next()
-          done()
         })
       )
-  })
-  it('transforms multiple files as Sass', (done) => {
-    gulp.src([
-      '__fixtures__/a.json',
-      '__fixtures__/props.yml'
-    ])
-    .pipe(theo.plugin(
-      {
-        transform: {
-          type: 'web'
-        },
-        format: {
-          type: 'scss'
-        }
-      }
-    ))
-    .pipe(
-      through.obj((file, enc, next) => {
-        if (file.relative === 'a.json') {
-          expect(file.contents.toString('utf8')).toMatchSnapshot()
-        }
-        console.log(file.relative)
-        if (file.relative === 'props.scss') {
-          expect(file.contents.toString('utf8')).toMatchSnapshot()
-          done()
-        }
-        next()
-      })
-    )
-  })
-  it('throws an error if a bad configuration is passed', (done) => {
-    gulp.src('__fixtures__/a.json')
-    .pipe(theo.plugin(
-      {
-        format: {
-          type: 'scss'
-        }
-      }
-    ))
-    .on('error', (e) => {
-      expect(e.message).toBe('Transform "undefined" is not registered')
-      done()
-    })
-  })
-
-  it('transforms Design Tokens as 1337v4r5', (done) => {
-    theo.registerFormat('1337v4r5', `
+      .pipe(
+        through.obj((file, enc, next) => {
+          expect(file.contents.toString('utf8')).toMatchSnapshot();
+          next();
+          done();
+        })
+      );
+  });
+  it('transforms multiple files as Sass', done => {
+    gulp
+      .src(['__fixtures__/a.json', '__fixtures__/props.yml'])
+      .pipe(
+        theo.plugin({
+          transform: {
+            type: 'web'
+          },
+          format: {
+            type: 'scss'
+          }
+        })
+      )
+      .pipe(
+        through.obj((file, enc, next) => {
+          if (file.relative === 'a.json') {
+            expect(file.contents.toString('utf8')).toMatchSnapshot();
+          }
+          if (file.relative === 'props.scss') {
+            expect(file.contents.toString('utf8')).toMatchSnapshot();
+            done();
+          }
+          next();
+        })
+      );
+  });
+  it('throws an error if a bad configuration is passed', done => {
+    gulp
+      .src('__fixtures__/a.json')
+      .pipe(
+        theo.plugin({
+          transform: {
+            type: undefined
+          },
+          format: {
+            type: 'scss'
+          }
+        })
+      )
+      .on('error', e => {
+        expect(e.message).toBe('Transform "undefined" is not registered');
+        done();
+      });
+  });
+  it('transforms Design Tokens as 1337v4r5', done => {
+    theo.registerFormat(
+      '1337v4r5',
+      `
       😀{{stem meta.file}}-1337:
         {{#each props as |prop|}}
-          '{{kebabCase prop.name}}': 😇({{prop.value}})
+          '{{kebabcase prop.name}}': 😇({{prop.value}})
         {{/each}}
-    `)
-    gulp.src('__fixtures__/a.json')
-    .pipe(theo.plugin(
-      {
-        transform: {
-          type: 'web'
-        },
-        format: {
-          type: '1337v4r5'
-        }
-      }
-    ))
-    .pipe(
-      through.obj((file, enc, next) => {
-        expect(file.contents.toString('utf8')).toMatchSnapshot()
-        next()
-        done()
-      })
-    )
-  })
-})
+    `
+    );
+    gulp
+      .src('__fixtures__/a.json')
+      .pipe(
+        theo.plugin({
+          transform: {
+            type: 'web'
+          },
+          format: {
+            type: '1337v4r5'
+          }
+        })
+      )
+      .pipe(
+        through.obj((file, enc, next) => {
+          expect(file.contents.toString('utf8')).toMatchSnapshot();
+          next();
+          done();
+        })
+      );
+  });
+});
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
-const through = require('through2')
-const gutil = require('gulp-util')
-const theo = require('theo')
-const Immutable = require('immutable')
+const through = require('through2');
+const gutil = require('gulp-util');
+const theo = require('theo');
 
 module.exports = Object.assign(
   {},
@@ -12,29 +11,33 @@ module.exports = Object.assign(
   theo,
   // Plugin:
   {
-    plugin: function (options) {
-      return through.obj(function (file, enc, callback) {
+    plugin: function(options) {
+      return through.obj(function(file, enc, callback) {
         if (file.isStream()) {
-          const err = this.emit('error', new gutil.PluginError('gulp-theo', 'Streaming not supported'))
-          return callback(err)
+          const err = this.emit(
+            'error',
+            new gutil.PluginError('gulp-theo', 'Streaming not supported')
+          );
+          return callback(err);
         }
-
-        const opts = Immutable.fromJS(options)
-          .setIn(['transform', 'file'], file.path)
-          .setIn(['transform', 'data'], file.contents.toString('utf8'))
-          .toJS()
-
-        theo.convert(opts)
+        const opts = Object.assign({}, options, {
+          transform: Object.assign({}, options.transform, {
+            file: file.path,
+            data: file.contents.toString('utf8')
+          })
+        });
+        theo
+          .convert(opts)
           .then(res => {
-            file.path = file.path.replace(/(json|yml)$/, options.format.type)
-            file.contents = Buffer.from(res)
-            callback(null, file)
+            file.path = file.path.replace(/(json|yml)$/, options.format.type);
+            file.contents = Buffer.from(res);
+            callback(null, file);
           })
           .catch(e => {
-            const err = new gutil.PluginError('gulp-theo', e, { trace: true })
-            callback(err, file)
-          })
-      })
+            const err = new gutil.PluginError('gulp-theo', e, { trace: true });
+            callback(err, file);
+          });
+      });
     }
   }
-)
+);
